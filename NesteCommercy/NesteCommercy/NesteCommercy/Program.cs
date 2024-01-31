8244614403a49c88825661ca5a6aac0ddbd181ae
tran tin <trantrongtin97@gmail.com>
2024-01-31 23:48:34 +0700

    Change project to WASM

--- NesteCommercy/NesteCommercy/NesteCommercy/Program.cs
+++ NesteCommercy/NesteCommercy/NesteCommercy/Program.cs
@@ -1,15 +1,20 @@
+using NesteCommercy.Client.Pages;
 using NesteCommercy.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -22,6 +27,7 @@ app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Counter).Assembly);
 
 app.Run();
